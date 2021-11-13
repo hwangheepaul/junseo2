e88dcd604143cc9d00e65ffdecd60b52714b3fed
Junseok Kim <junseokkim00@gmail.com>
2021-11-13 21:00:15 +0900

    지금까지 짰던 코드

---
+++ Essential_module.py
@@ -0,0 +1,5 @@
+# 많이 사용되는 값들을 모아놓은 모듈
+width = 320
+height = 240
+pixelSize = 100
+Plate_matrix = [[0]*height]*width
---
+++ Studentclass.py
@@ -0,0 +1,31 @@
+from Essential_module import *
+
+# height와 width 값 이상하면 순서 바꾸기
+
+class Student:
+    firstMatrix=[[0]*height]*width
+    secondMatrix=[[0]*height]*width
+    def __init__(self,studentNumber):
+        self.studentNumber = studentNumber
+    def saveFirstMatrix(self,firstMatrix):
+        for x in range(width):
+            for y in range(height):
+                self.firstMatrix[x][y] = firstMatrix[x][y]
+    def saveSecondMatrix(self,secondMatrix):
+        for x in range(width):
+            for y in range(height):
+                self.secondMatrix[x][y] = secondMatrix[x][y]
+    
+    def calculateFirstVolume(self):
+        Volume=0
+        for x in range(width):
+            for y in range(height):
+                Volume+=(Plate_matrix[x][y]-self.firstMatrix[x][y])*pixelSize
+        return Volume
+    
+    def calculateSecondVolume(self):
+        Volume=0
+        for x in range(width):
+            for y in range(height):
+                Volume+=(self.secondMatrix[x][y]-self.firstMatrix[x][y])*pixelSize
+        return Volume
